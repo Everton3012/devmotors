f010722a22f8ab498aa99d09436aa259f3189450
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-05-22 22:58:16 -0300

    efeito na barra de navegação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <main>
-      <h1>TESTE PAGINA</h1>
+      <div style={{width: "100%", height: 600, backgroundColor:"#121212", marginBottom: 24}}>
+        <h1>Teste</h1>
+      </div>
+      <div style={{width: "100%", height: 600, backgroundColor:"blue", marginBottom: 24}}>
+        <h1>Teste</h1>
+      </div>
+      <div style={{width: "100%", height: 600, backgroundColor:"green", marginBottom: 24}}>
+        <h1>Teste</h1>
+      </div>
     </main>
   );
 }
